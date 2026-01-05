37d7b411b1cd4c451962c06758d02cc9a9b8a1e5
LiChunlin <160166711+shmbatom@users.noreply.github.com>
2026-01-05 20:16:23 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,7 +2,12 @@
 
 from .nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 
-# 告诉 ComfyUI 前端资源位置
+# ComfyUI 前端资源位置
 WEB_DIRECTORY = "./js"
 
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS", "WEB_DIRECTORY"]
+
+# 插件元信息（ComfyUI Manager识别用）
+__version__ = "1.0.0"
+__author__ = "李春林/leesir"
+__description__ = "image split comparison horizontally/vertically, real-time mouse interaction to adjust split ratios, bidirectional linkage between slider and mouse, and the ability to export comparison images with precise split lines."
